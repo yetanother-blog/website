e984e6bda48716c4a0bd7c1a3b1a5cb8ff838937
andrew-secret <andre.rusakow@jimdo.com>
2020-03-31 23:14:21 +0200

    feat: implement theme provider 💅

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,9 @@
 import { Link } from 'gatsby';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { rhythm, styledScale } from '../utils/typography';
+import { theme } from '../utils/theme';
+import { Box } from './Box/Box';
 
 interface Props {
   title: string;
@@ -19,25 +21,32 @@ const Content = styled.div`
   padding: ${`${rhythm(1.5)} ${rhythm(3 / 4)}`};
 `;
 
+export const MyComponent = styled.div`
+  color: ${(props) => props.theme.colors.primary};
+`;
+
 export const Layout: React.FC<Props> = ({ title, children }) => {
   const rootPath = `/`;
 
   return (
-    <Content>
-      <header>
-        <StyledH1>
-          <Link to={rootPath}>{title}</Link>
-        </StyledH1>
-        <nav>
-          <Link to="/repos">Repos</Link>
-          <Link to="/guides">Guide</Link>
-        </nav>
-      </header>
-      <main>{children}</main>
-      <footer>
-        <Link to="/about">About</Link>
-        <Link to="/imprint">Imprint</Link>
-      </footer>
-    </Content>
+    <ThemeProvider theme={theme}>
+      <Content>
+        <header>
+          <StyledH1>
+            <Link to={rootPath}>{title}</Link>
+          </StyledH1>
+          <Box role="presentation">lolikopter</Box>
+          <nav>
+            <Link to="/repos">Repos</Link>
+            <Link to="/guides">Guide</Link>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>
+          <Link to="/about">About</Link>
+          <Link to="/imprint">Imprint</Link>
+        </footer>
+      </Content>
+    </ThemeProvider>
   );
 };
