084019bed34b8b1a687eb5b1306a69eb39ddcd1d
andrew-secret <andre.rusakow@jimdo.com>
2020-04-05 19:56:50 +0200

    refactor: adjust color and fontSize in theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,13 +1,13 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  fontSizes: [12, 14, 16, 20, 24, 32],
+  fontSizes: ['12px', '14px', '16px', '20px', '24px', '32px'],
   colors: {
     primary: '#00FF00',
     secondary: '#FB00FF',
     grey500: '#1C1C1C',
     grey400: '#ACADAC',
-    grey300: '#FBFBFB',
+    grey300: '#F6F6F6',
   },
   breakpoints: ['576px', '768px', '992px', '1200px'],
   space: {
