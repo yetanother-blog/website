1cb6286932a1a49eaf8c43b750108eb9790b7099
Henrik Fricke <henrikfricke@web.de>
2020-04-16 13:33:31 +0200

    feat: add cookie law banner

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,6 +8,7 @@ import { Twitter } from './SocialLink/Twitter';
 import { Github } from './SocialLink/Github';
 import { Box } from './Box/Box';
 import { MobileNavigationContext } from '../context/MobileNavigationContext/MobileNavigationContext';
+import { CookieLawBanner } from './CookieLawBanner/CookieLawBanner';
 
 interface LayoutProps {
   size?: 'narrow';
@@ -28,74 +29,93 @@ export const Layout: React.FC<LayoutProps> = ({ children, size }) => {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      height="100vh"
-      m="0 auto"
-      maxWidth="940px"
-      pl={theme.space.l}
-      pr={theme.space.l}
-      overflow={isMobileNavigationOpen ? 'hidden' : 'visible'}
-    >
-      <Navigation>
-        <Link
-          variant="tertiary"
-          component={GatsbyLink}
-          onClick={handleMobileNavigation}
-          to="/repos"
-          partiallyActive
-          marginBottom={[theme.space.l, 0]}
-        >
-          Repos
-        </Link>
-        <Link
-          variant="tertiary"
-          component={GatsbyLink}
-          onClick={handleMobileNavigation}
-          to="/guides"
-          marginBottom={[theme.space.l, 0]}
-        >
-          Guide
-        </Link>
-        <SocialLink href="https://twitter.com/_yetanotherblog">
-          <Twitter />
-        </SocialLink>
-        <SocialLink href="https://github.com/yetanother-blog">
-          <Github />
-        </SocialLink>
-      </Navigation>
+    <>
+      <CookieLawBanner />
       <Box
-        as="main"
-        width="100%"
-        flex="1 0 auto"
-        maxWidth={isNarrow ? '640px' : '940px'}
-      >
-        {children}
-      </Box>
-      <Box
-        as="footer"
         display="flex"
-        justifyContent="flex-end"
+        flexDirection="column"
+        alignItems="center"
+        height="100vh"
+        m="0 auto"
         maxWidth="940px"
-        width="100%"
-        pt={theme.space.xl}
-        pb={theme.space.xl}
+        pl={theme.space.l}
+        pr={theme.space.l}
+        overflow={isMobileNavigationOpen ? 'hidden' : 'visible'}
       >
-        <Link
-          display="flex"
-          variant="quaternary"
-          component={GatsbyLink}
-          to="/about"
-          mr={theme.space.l}
+        <Navigation>
+          <Link
+            variant="tertiary"
+            component={GatsbyLink}
+            onClick={handleMobileNavigation}
+            to="/repos"
+            partiallyActive
+            marginBottom={[theme.space.l, 0]}
+          >
+            Repos
+          </Link>
+          <Link
+            variant="tertiary"
+            component={GatsbyLink}
+            onClick={handleMobileNavigation}
+            to="/guides"
+            marginBottom={[theme.space.l, 0]}
+          >
+            Guide
+          </Link>
+          <SocialLink href="https://twitter.com/_yetanotherblog">
+            <Twitter />
+          </SocialLink>
+          <SocialLink href="https://github.com/yetanother-blog">
+            <Github />
+          </SocialLink>
+        </Navigation>
+        <Box
+          as="main"
+          width="100%"
+          flex="1 0 auto"
+          maxWidth={isNarrow ? '640px' : '940px'}
         >
-          About
-        </Link>
-        <Link variant="quaternary" component={GatsbyLink} to="/legal-details">
-          Legal Details
-        </Link>
+          {children}
+        </Box>
+        <Box
+          as="footer"
+          display="flex"
+          justifyContent="flex-end"
+          maxWidth="940px"
+          width="100%"
+          pt={theme.space.xl}
+          pb={theme.space.xl}
+        >
+          <Link
+            display="flex"
+            variant="quaternary"
+            component={GatsbyLink}
+            to="/about"
+            mr={theme.space.l}
+          >
+            About
+          </Link>
+          <Link
+            variant="quaternary"
+            href="https://www.iubenda.com/privacy-policy/46807218"
+            target="_blank"
+            mr={theme.space.l}
+          >
+            Privacy Policy
+          </Link>
+          <Link
+            variant="quaternary"
+            href="https://www.iubenda.com/privacy-policy/46807218/cookie-policy"
+            target="_blank"
+            mr={theme.space.l}
+          >
+            Cookie Policy
+          </Link>
+          <Link variant="quaternary" component={GatsbyLink} to="/legal-details">
+            Legal Details
+          </Link>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
