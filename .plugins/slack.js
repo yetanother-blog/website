fb5a7f3ee0caa0c8f1c925c402242c67ceb583c8
Henrik Fricke <henrikfricke@web.de>
2020-04-26 23:33:06 +0200

    refactor: use title of the commit

--- .plugins/slack.js
+++ .plugins/slack.js
@@ -1,32 +1,86 @@
 const fetch = require('node-fetch');
 
-module.exports = {
-  async onEnd({ utils, error }) {
-    console.log(JSON.stringify(utils.git.commits));
+const SLACK_CHANNEL = 'C012K6XJEH1';
 
-    let text = '*Whoop Whoop new release for yetanother.blog! 🎉* \n\n';
-    text += 'Commits: \n';
-    text += utils.git.commits
-      .map(
-        (commit) =>
-          `- ${commit.message} by _${
-            commit.committer.name
-          }_ (<https://github.com/yetanother-blog/website/commit/${
-            commit.sha
-          }|${commit.sha.substring(0, 7)}>)`
-      )
-      .join('\n');
+const sendSlackMessage = async ({ text, attachments = [], blocks = [] }) => {
+  return fetch('https://slack.com/api/chat.postMessage', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_DEPLOYMENT_BOT_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      channel: SLACK_CHANNEL,
+      text,
+      attachments,
+      blocks,
+    }),
+  });
+};
 
-    await fetch('https://slack.com/api/chat.postMessage', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.SLACK_DEPLOYMENT_BOT_TOKEN}`,
-        'Content-Type': 'application/json',
+const onSuccess = async ({ utils }) => {
+  const commits = await Promise.all(
+    utils.git.commits.map(async (commit) => {
+      const message = await utils.run.command(
+        `git show --pretty=format:%s -s ${commit.sha}`
+      );
+
+      return {
+        message: message.stdout,
+        sha1: commit.sha.substring(0, 7),
+        link: `${process.env.REPOSITORY_URL}/commit/${commit.sha}`,
+        committer: commit.committer.name,
+      };
+    })
+  );
+
+  const attachments = commits.map((commit) => ({
+    color: '#36a64f',
+    text: `${commit.message} by ${commit.committer} (<${commit.link}|${commit.sha1}>)`,
+  }));
+
+  await sendSlackMessage({
+    text: '*Whoop Whoop new release for yetanother.blog! 🎉*',
+    attachments,
+  });
+};
+
+const onError = async () => {
+  await sendSlackMessage({
+    text: '🚨🚨🚨 *Deployment failed for yetanother.blog!* 🚨🚨🚨',
+    blocks: [
+      {
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: '🚨🚨🚨 *Deployment failed for yetanother.blog!* 🚨🚨🚨',
+          },
+        ],
       },
-      body: JSON.stringify({
-        channel: 'C012K6XJEH1',
-        text,
-      }),
-    });
-  },
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'View logs',
+            },
+            url: `https://app.netlify.com/sites/yetanotherblog/deploys/${process.env.DEPLOY_ID}`,
+          },
+        ],
+      },
+    ],
+  });
+};
+
+module.exports = () => {
+  console.log('CONTEXT', process.env.CONTEXT);
+
+  if (process.env.CONTEXT !== 'production') {
+    return {};
+  }
+
+  return { onSuccess, onError };
 };
