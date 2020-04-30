2db3a60a7bd4935b0d650d5dab2b2dc4abb44a1a
André Rusakow <andre.rusakow@googlemail.com>
2020-04-30 21:49:04 +0200

    feat(teaser): add more mobile adjustments

--- src/components/Teaser/Teaser.tsx
+++ src/components/Teaser/Teaser.tsx
@@ -5,10 +5,6 @@ import { Box } from '../Box/Box';
 import Browser from './Browser.svg';
 import { Typography } from '../Typography/Typography';
 
-const StyledBackground = styled(Background)`
-  color: ${(props) => props.theme.colors.grey300};
-`;
-
 const rotate = keyframes`
   0%, 100% {
     transform: translate(40px, -50px);
@@ -23,13 +19,32 @@ const StyledWrapper = styled.span`
   align-items: center;
   justify-content: center;
   position: relative;
-  margin-bottom: ${(props) => props.theme.space.l}px;
+
+  @media (min-width: ${(props) => props.theme.breakpoints[0]}) {
+    margin-bottom: ${(props) => props.theme.space.l}px;
+  }
+`;
+
+const StyledBackground = styled(Background)`
+  color: ${(props) => props.theme.colors.grey300};
+  width: 322px;
+  height: 246px;
+
+  @media (min-width: ${(props) => props.theme.breakpoints[0]}) {
+    width: 422px;
+    height: 346px;
+  }
 `;
 
 const StyledBrowser = styled(Browser)`
   position: absolute;
   transform: translate(40px, -50px);
   animation: ${rotate} 10s linear infinite;
+  width: 200px;
+
+  @media (min-width: ${(props) => props.theme.breakpoints[0]}) {
+    width: 400px;
+  }
 `;
 
 const StyledTypographyWrapper = styled.div`
@@ -39,14 +54,16 @@ const StyledTypographyWrapper = styled.div`
   padding: 10px;
 
   &:after {
-    content: '';
-    background-color: ${(props) => props.theme.colors.secondary};
-    position: absolute;
-    bottom: -6px;
-    left: -6px;
-    width: 100%;
-    height: calc(100% - 50px);
-    z-index: -1;
+    @media (min-width: ${(props) => props.theme.breakpoints[0]}) {
+      content: '';
+      background-color: ${(props) => props.theme.colors.secondary};
+      position: absolute;
+      bottom: -6px;
+      left: -6px;
+      width: 100%;
+      height: calc(100% - 50px);
+      z-index: -1;
+    }
   }
 `;
 
