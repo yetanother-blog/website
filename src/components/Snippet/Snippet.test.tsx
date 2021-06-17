428f3a29531dfa65ff6a0a7eaca8f8749655af34
Henrik Fricke <henrikfricke@web.de>
2021-06-17 10:24:50 +0200

    refactor: introduce indiepen for our blogposts

--- src/components/Snippet/Snippet.test.tsx
+++ src/components/Snippet/Snippet.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Snippet } from './Snippet';
+import { ThemeContext } from 'styled-components';
+import { theme } from '../../utils/theme';
 
 describe('Snippet component', () => {
   it('should render', () => {
     const { container } = render(
-      <Snippet
-        src="data:text/html;charset=utf-8,%3Chtml%3E%3Cbody%3Efoo%3C/body%3E%3C/html%3E"
-        title="Title is coooool :)"
-        height="256"
-      />
+      <ThemeContext.Provider value={theme}>
+        <Snippet
+          url="https://example.com"
+          title="Title is coooool :)"
+          height={256}
+        />
+      </ThemeContext.Provider>
     );
 
     expect(container).toMatchSnapshot();
