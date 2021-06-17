cf0be692ffd3821c9a19be7633d1036494dc47be
Henrik Fricke <henrikfricke@web.de>
2021-06-17 13:23:15 +0200

    refactor: introduce indiepen for our blogposts (#925)

--- src/components/Snippet/Snippet.tsx
+++ src/components/Snippet/Snippet.tsx
@@ -1,9 +1,40 @@
+import React from 'react';
 import styled from 'styled-components';
 
-export const Snippet = styled.iframe.attrs(() => ({
-  loading: 'lazy',
-}))`
-  width: 100%;
-  margin-bottom: 0;
+export interface SnipperProps {
+  url: string;
+  title: string;
+  height: number;
+  defaultTab?: 'result' | 'html' | 'css' | 'js';
+}
+
+export const StyledSnippet = styled.iframe`
+  display: block;
+  overflow: hidden;
   border: 0;
+  margin-bottom: ${(props) => props.theme.space.m}px;
+
+  @media (min-width: 992px) {
+    width: 780px;
+    margin-left: -50px;
+  }
 `;
+
+export const Snippet: React.FC<SnipperProps> = ({
+  url,
+  title,
+  height,
+  defaultTab = 'html',
+}) => {
+  const encodedURL = encodeURIComponent(url);
+
+  return (
+    <StyledSnippet
+      src={`https://indiepen.tech/embed/?url=${encodedURL}&tab=${defaultTab}`}
+      loading="lazy"
+      title={title}
+      width="100%"
+      height={height}
+    />
+  );
+};
