371ce6e8e40e0512b40eaf65ad8256ec76788d13
Henrik Fricke <henrikfricke@web.de>
2021-07-08 21:28:06 +0200

    refactor: init eleventy

--- src/context/ThemeProvider.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { theme } from '../utils/theme';
-
-export const ThemeContextProvider: React.FC = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
