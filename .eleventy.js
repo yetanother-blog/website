995e0fe674f35759c516ba334c6c54e60e9b1a8b
Henrik Fricke <henrikfricke@web.de>
2021-07-09 13:51:50 +0200

    feat: navigation

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,14 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const externalLinks = require('eleventy-plugin-external-links')
 
 module.exports = function(eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(externalLinks);
 
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
