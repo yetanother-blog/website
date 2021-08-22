9b1bd1a398279a88da9a76ae5fffbe882db58c0c
Henrik Fricke <henrikfricke@web.de>
2021-08-22 19:34:23 +0200

    fix: rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,10 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL dd, yyyy");
   });
 
+  eleventyConfig.addFilter("rssDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toHTTP();
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
